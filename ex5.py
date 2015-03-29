35409c72a7f8cf639a5b1952deca7dfb9ff6f959
shepp67 <shepp67@gmail.com>
2015-03-29 18:12:13 -0400

    Ex5 practice

--- ex5.py
+++ ex5.py
@@ -5,21 +5,24 @@
 # -*- coding: utf-8 -*-
 
 # Begin setting up variables
-my_name = 'Joel F. Sheppard'
-my_age = 47 #not a lie
-my_height = 73 #inches
-my_weight = 187 #lbs
-my_eyes = 'Brown'
-my_teeth = 'white'
-my_hair = 'Brown'
+name = 'Joel F. Sheppard'
+age = 47 #not a lie
+height = 73.0 #inches
+weight = 187 #lbs
+eyes = 'Brown'
+teeth = 'white'
+hair = 'Brown'
+metric_conversion = 2.74
 
 # Print statements using variables set up above
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+#print "He's %d centimeters tall." % (height * metric_conversion)
+print "He's %f centimeters tall." % (height * metric_conversion)
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "his teeth are usually %s depending on the coffee" % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+#print "He's got %r eyes and %r hair." % (eyes, hair)
+print "his teeth are usually %s depending on the coffee" % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d  I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d  I get %d." % (age, height, weight, age + height + weight)
